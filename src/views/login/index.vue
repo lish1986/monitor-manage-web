add02317909e15f48c9d5de90310a241acbdea6d
Sheng <js.nj.lisheng@outlook.com>
2020-10-22 22:31:20 +0800

    【增加登录方式】

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,136 +1,288 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <div class="title-container">
+      <h3 class="title">监控管理系统</h3>
+    </div>
+    <div class="login-form">
+      <el-tabs value="message" @tab-click="handleTabChange">
+        <el-tab-pane label="短信登录" name="message">
+          <el-form
+            ref="messageLoginForm"
+            :model="messageLoginForm"
+            :rules="messageLoginRules"
+            auto-complete="on"
+            label-position="left"
+          >
+            <el-form-item prop="phoneNumber">
+              <span class="svg-container">
+                <svg-icon icon-class="user" />
+              </span>
+              <el-input
+                ref="phoneNumber"
+                v-model="messageLoginForm.phoneNumber"
+                placeholder="请输入手机号"
+                name="phoneNumber"
+                type="text"
+                tabindex="1"
+                auto-complete="on"
+              />
+            </el-form-item>
+            <el-form-item prop="verifyCode">
+              <span class="svg-container">
+                <svg-icon icon-class="message" />
+              </span>
+              <el-input
+                ref="verifyCode"
+                v-model="messageLoginForm.verifyCode"
+                placeholder="请输入验证码"
+                name="verifyCode"
+                type="text"
+                tabindex="2"
+                auto-complete="on"
+                class="verify-input"
+              />
+              <el-button @click="getVerifyCode" :disabled="verifyCodeSended"
+                >获取验证码</el-button
+              >
+            </el-form-item>
+            <div class="tips" v-if="verifyCodeSended">
+              <span>短信验证码已发送, 可能会有延后请耐心等待</span>
+            </div>
 
-      <div class="title-container">
-        <h3 class="title">监控管理系统</h3>
-      </div>
+            <el-button
+              :loading="loading"
+              type="primary"
+              style="width: 100%"
+              @click.native.prevent="handleLogin"
+              >登录</el-button
+            >
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="密码登录" name="password">
+          <el-form
+            ref="passwordLoginForm"
+            :model="passwordLoginForm"
+            :rules="passwordLoginRules"
+            auto-complete="on"
+            label-position="left"
+          >
+            <el-form-item prop="username">
+              <span class="svg-container">
+                <svg-icon icon-class="user" />
+              </span>
+              <el-input
+                ref="username"
+                v-model="passwordLoginForm.username"
+                placeholder="请输入用户名"
+                name="username"
+                type="text"
+                tabindex="1"
+                auto-complete="on"
+              />
+            </el-form-item>
+            <el-form-item prop="password">
+              <span class="svg-container">
+                <svg-icon icon-class="password" />
+              </span>
+              <el-input
+                :key="passwordType"
+                ref="password"
+                v-model="passwordLoginForm.password"
+                :type="passwordType"
+                placeholder="请输入密码"
+                name="password"
+                tabindex="2"
+                auto-complete="on"
+                @keyup.enter.native="handleLogin"
+              />
+              <span class="show-pwd" @click="showPwd">
+                <svg-icon
+                  :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+                />
+              </span>
+            </el-form-item>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
-
-      <!-- <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div> -->
-
-    </el-form>
+            <el-button
+              :loading="loading"
+              type="primary"
+              style="width: 100%"
+              @click.native.prevent="handleLogin"
+              >登录</el-button
+            >
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { validUsername } from "@/utils/validate";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error("请输入正确的用户名"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error("密码长度不能低于6位"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
+
+    const validatePhoneNumber = (rule, value, callback) => {
+      if (!value || value == "") {
+        callback(new Error("请输入手机号"));
+      } else {
+        callback();
+      }
+    };
+    const validateVerifyCode = (rule, value, callback) => {
+      if (!value || value == "") {
+        callback(new Error("请输入验证码"));
+      } else {
+        callback();
+      }
+    };
     return {
-      loginForm: {
-        username: 'admin',
-        password: '111111'
+      messageLoginForm: {
+        phoneNumber: "",
+        verifyCode: "",
       },
-      loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+      passwordLoginForm: {
+        username: "",
+        password: "",
+      },
+      passwordLoginRules: {
+        username: [
+          {
+            required: true,
+            trigger: "blur",
+            validator: validateUsername,
+          },
+        ],
+        password: [
+          {
+            required: true,
+            trigger: "blur",
+            validator: validatePassword,
+          },
+        ],
+      },
+      loginType: "",
+      messageLoginRules: {
+        phoneNumber: [
+          {
+            required: true,
+            trigger: "blur",
+            validator: validatePhoneNumber,
+          },
+        ],
+        verifyCode: [
+          {
+            required: true,
+            trigger: "blur",
+            validator: validateVerifyCode,
+          },
+        ],
       },
       loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
+      verifyCodeSended: false,
+      passwordType: "password",
+      redirect: undefined,
+      tipsText: "",
+    };
   },
   watch: {
     $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
+      handler: function (route) {
+        this.redirect = route.query && route.query.redirect;
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   methods: {
+    // 获取验证码
+    getVerifyCode() {
+      console.log("verify code sended...");
+      this.verifyCodeSended = true;
+    },
+    // toggle密码
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password'
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+        this.$refs.password.focus();
+      });
+    },
+    handleTabChange(tab) {
+      this.loginType = tab.name;
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    }
-  }
-}
+      // 选择短信登录
+      if (this.loginType == "message") {
+        this.$refs.messageLoginForm.validate((valid) => {
+          if (valid) {
+            this.loading = true;
+            this.$store
+              .dispatch("user/login", this.messageLoginForm)
+              .then(() => {
+                this.$router.push({
+                  path: this.redirect || "/",
+                });
+                this.loading = false;
+              })
+              .catch(() => {
+                this.loading = false;
+              });
+          } else {
+            console.log("用户输入有误!!");
+            return false;
+          }
+        });
+      } else {
+        // 选择用户名+密码登录
+        this.$refs.passwordLoginForm.validate((valid) => {
+          if (valid) {
+            this.loading = true;
+            this.$store
+              .dispatch("user/login", this.passwordLoginForm)
+              .then(() => {
+                this.$router.push({
+                  path: this.redirect || "/",
+                });
+                this.loading = false;
+              })
+              .catch(() => {
+                this.loading = false;
+              });
+          } else {
+            console.log("用户输入有误!!");
+            return false;
+          }
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -163,6 +315,15 @@ $cursor: #fff;
     }
   }
 
+  .el-tabs__item {
+    &.is-active {
+      color: #409eff;
+    }
+
+    color: white;
+    font-size: 16px;
+  }
+
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
     background: rgba(0, 0, 0, 0.1);
@@ -170,12 +331,11 @@ $cursor: #fff;
     color: #454545;
   }
 }
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+</style
+><style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
@@ -185,23 +345,27 @@ $light_gray:#eee;
 
   .login-form {
     position: relative;
-    width: 520px;
+    width: 400px;
     max-width: 100%;
     padding: 160px 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
 
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
+  .verify-input {
+    width: 55%;
+  }
 
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
+  .tips {
+    font-size: 12px;
+    color: #fff;
+    margin-bottom: 20px;
+
+    // span {
+    //   &:first-of-type {
+    //     margin-right: 16px;
+    //   }
+    // }
   }
 
   .svg-container {
@@ -214,6 +378,7 @@ $light_gray:#eee;
 
   .title-container {
     position: relative;
+    top: 150px;
 
     .title {
       font-size: 26px;
